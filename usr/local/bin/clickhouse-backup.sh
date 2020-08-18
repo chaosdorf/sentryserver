374787be92b69b258c3715613b78d4f240c37199
Niklas Sombert <niklas@ytvwld.de>
2020-08-18 16:07:19 +0200

    dash doesn't support pipefail?

--- usr/local/bin/clickhouse-backup.sh
+++ usr/local/bin/clickhouse-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -euo pipefail
+set -eu
 DATE=$(date -I)
 cd /var/lib/clickhouse-backup
 clickhouse client -q "SELECT * FROM sentry_local INTO OUTFILE 'sentry_local-$DATE.csv' FORMAT CSVWithNames"
