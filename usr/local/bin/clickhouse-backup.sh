ee9726d43e806baed76b3eefab75eb53c16b3536
Niklas Sombert <niklas@ytvwld.de>
2020-10-06 13:25:21 +0200

    Compress backups with bzip2

--- usr/local/bin/clickhouse-backup.sh
+++ usr/local/bin/clickhouse-backup.sh
@@ -3,4 +3,5 @@ set -eu
 DATE=$(date -I)
 cd /var/lib/clickhouse-backup
 clickhouse client -q "SELECT * FROM sentry_local INTO OUTFILE 'sentry_local-$DATE.csv' FORMAT CSVWithNames"
+bzip2 sentry_local-$DATE.csv
 clickhouse client -q "SHOW CREATE TABLE sentry_local" > schema-$DATE.sql
